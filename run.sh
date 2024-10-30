2b2231c440a7afb8ce7ef0073c963b0dd19072c0
rwallac2 <rwallac2@uccs.edu>
2024-10-29 20:06:45 -0600

    FIxing up for MNIST

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 clear
-python3 trainimagenet.py --data DataSets/imagenet-1k | tee -a log/log.txt
+# python3 trainimagenet.py --data DataSets/imagenet-1k | tee -a log/log.txt
+python3 trainMnist.py --data DataSets/MNIST | tee -a log/log.txt
